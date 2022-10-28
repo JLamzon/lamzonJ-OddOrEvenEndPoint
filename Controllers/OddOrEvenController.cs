ea83b987d22f3916f9ce58c52cd03f3a3348e5ba
JLamzon <jlamzon@yahoo.com>
2022-10-27 18:06:10 -0700

    update notes

--- Controllers/OddOrEvenController.cs
+++ Controllers/OddOrEvenController.cs
@@ -3,7 +3,7 @@
 //Odd or Even End Point
 //Have endpoints configured to complete Mini Challenge # 6
 //Peer review: BY N. HARRISON WILKINS : I LIKE THE DATA VALIDATION, CODE IS EASY TO READ AND VERY DIGESTABLE.   OVERALL VERY NICE, VERY NEAT WORK!!! GREAT JOB!
-//
+
 
 using Microsoft.AspNetCore.Mvc;
 
